fcd8db6001af70837f65cb598cec1f1d06de86d3
outaTiME <afalduto@gmail.com>
2012-09-12 12:42:13 -0300

    Version 0.2.0 bump. General fixes and test cases added.

--- grunt.js
+++ grunt.js
@@ -15,89 +15,94 @@
  */
 
 module.exports = function (grunt) {
+  'use strict';
 
+  // Project configuration.
   grunt.initConfig({
+
     lint: {
-      files: [
-        'grunt.js',
-        'tasks/**/*.js'
-      ]
+      all: ['grunt.js', 'tasks/*.js', '<config:nodeunit.tasks>']
     },
-    watch: {
-      files: '<config:lint.files>',
-      tasks: 'default'
-    },
+
     jshint: {
       options: {
-
-        // Settings
-        "passfail"      : false,  // Stop on first error.
-        "maxerr"        : 100,    // Maximum errors before stopping.
-        "indent"        : 2,
-
-        // Predefined globals whom JSHint will ignore.
-        "browser"       : true,   // Standard browser globals e.g. `window`, `document`.
-
-        "node"          : true,
-        "rhino"         : false,
-        "couch"         : false,
-        "wsh"           : true,   // Windows Scripting Host.
-
-        "jquery"        : true,
-        "prototypejs"   : false,
-        "mootools"      : false,
-        "dojo"          : false,
-
-        // Development.
-        "debug"         : false,  // Allow debugger statements e.g. browser breakpoints.
-        "devel"         : true,   // Allow development statements e.g. `console.log();`.
-
-        // EcmaScript 5.
-        "es5"           : true,   // Allow EcmaScript 5 syntax.
-        "strict"        : false,  // Require `use strict` pragma in every file.
-        "globalstrict"  : false,  // Allow global "use strict" (also enables 'strict').
-
-        // The Good Parts.
-        "asi"           : false,  // Tolerate Automatic Semicolon Insertion (no semicolons).
-        "laxbreak"      : true,   // Tolerate unsafe line breaks e.g. `return [\n] x` without semicolons.
-        "bitwise"       : true,   // Prohibit bitwise operators (&, |, ^, etc.).
-        "boss"          : false,  // Tolerate assignments inside if, for & while. Usually conditions & loops are for comparison, not assignments.
-        "curly"         : true,   // Require {} for every new block or scope.
-        "eqeqeq"        : true,   // Require triple equals i.e. `===`.
-        "eqnull"        : false,  // Tolerate use of `== null`.
-        "evil"          : false,  // Tolerate use of `eval`.
-        "expr"          : false,  // Tolerate `ExpressionStatement` as Programs.
-        "forin"         : false,  // Tolerate `for in` loops without `hasOwnPrototype`.
-        "immed"         : true,   // Require immediate invocations to be wrapped in parens e.g. `( function(){}() );`
-        "latedef"       : true,   // Prohibit variable use before definition.
-        "loopfunc"      : false,  // Allow functions to be defined within loops.
-        "noarg"         : true,   // Prohibit use of `arguments.caller` and `arguments.callee`.
-        "regexp"        : true,   // Prohibit `.` and `[^...]` in regular expressions.
-        "regexdash"     : false,  // Tolerate unescaped last dash i.e. `[-...]`.
-        "scripturl"     : true,   // Tolerate script-targeted URLs.
-        "shadow"        : false,  // Allows re-define variables later in code e.g. `var x=1; x=2;`.
-        "supernew"      : false,  // Tolerate `new function () { ... };` and `new Object;`.
-        "undef"         : true,   // Require all non-global variables be declared before they are used.
-
-        // Persone styling prefrences.
-        "newcap"        : true,   // Require capitalization of all constructor functions e.g. `new F()`.
-        "noempty"       : true,   // Prohibit use of empty blocks.
-        "nonew"         : true,   // Prohibit use of constructors for side-effects.
-        "nomen"         : true,   // Prohibit use of initial or trailing underbars in names.
-        "onevar"        : false,  // Allow only one `var` statement per function.
-        "plusplus"      : false,  // Prohibit use of `++` & `--`.
-        "sub"           : false,  // Tolerate all forms of subscript notation besides dot notation e.g. `dict['key']` instead of `dict.key`.
-        "trailing"      : true,   // Prohibit trailing whitespaces.
-        "white"         : true    // Check against strict whitespace and indentation rules.
-
-      },
-      globals: {
+        curly: true,
+        eqeqeq: true,
+        immed: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        sub: true,
+        undef: true,
+        boss: true,
+        eqnull: true,
+        node: true,
+        es5: true
       }
+    },
+
+    // Before generating any new files, remove any previously-created files.
+    clean: {
+      test: ['tmp']
+    },
+
+    // Configuration to be run (and then tested).
+    replace: {
+      simple: {
+        src: ['test/fixtures/simple.txt'],
+        dest: 'tmp',
+        variables: {
+          key: 'value'
+        }
+      },
+      prefix: {
+        src: ['test/fixtures/prefix.txt'],
+        dest: 'tmp',
+        variables: {
+          key: 'value'
+        },
+        prefix: '@replace:'
+      },
+      dynamic_key: {
+        src: ['test/fixtures/dynamic_key.txt'],
+        dest: 'tmp',
+        variables: {
+          '<%= "key" %>': 'value'
+        }
+      },
+      dynamic_value: {
+        src: ['test/fixtures/dynamic_value.txt'],
+        dest: 'tmp',
+        variables: {
+          key: '<%= grunt.template.today("yyyy") %>'
+        }
+      }
+    },
+
+    // Unit tests.
+    nodeunit: {
+      tasks: ['test/*_test.js']
+    },
+
+    watch: {
+      files: '<config:lint.all>',
+      tasks: 'default'
     }
+
   });
 
+  // Actually load this plugin's task(s).
   grunt.loadTasks('tasks');
 
-  grunt.registerTask('default', 'lint');
+  // The clean plugin helps in testing.
+  grunt.loadNpmTasks('grunt-contrib-clean');
+
+  // Whenever the 'test' task is run, first clean the 'tmp' dir, then run this
+  // plugin's task(s), then test the result.
+  grunt.renameTask('test', 'nodeunit');
+  grunt.registerTask('test', 'clean replace nodeunit');
+
+  // By default, lint and run all tests.
+  grunt.registerTask('default', 'lint test');
 
 };
---
+++ test/replace_test.js
@@ -0,0 +1,29 @@
+var grunt = require('grunt');
+
+exports['replace'] = {
+  main: function(test) {
+    'use strict';
+
+    var expect, result;
+
+    test.expect(4);
+
+    expect = 'value\n';
+    result = grunt.file.read('tmp/simple.txt');
+    test.equal(expect, result, 'should replace simple key with value');
+
+    expect = 'value\n';
+    result = grunt.file.read('tmp/prefix.txt');
+    test.equal(expect, result, 'should replace simple key with value using custom prefix');
+
+    expect = 'value\n';
+    result = grunt.file.read('tmp/dynamic_key.txt');
+    test.equal(expect, result, 'should replace templated key with defined value');
+
+    expect = grunt.template.today('yyyy') + "\n";
+    result = grunt.file.read('tmp/dynamic_value.txt');
+    test.equal(expect, result, 'should replace simple key with templated value');
+
+    test.done();
+  }
+};
